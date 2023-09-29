71d436be61a7471d472fff6addb408d67bcccea3
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2023-09-29 17:29:51 +0300

    Add files via upload

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(arg){
+    let result = Number.parseFloat(arg);
+    const parsingerror = new Error("Невалидное значение");
+    if(isNaN(result)){
+    throw parsingerror;
+    }
+    return result;
+}
+function validateCount(arg){
+    try{
+        if(parseCount(arg)){
+            return parseCount(arg);
+        };
+    }
+    catch(error){
+        return error;
+    }
+};
+class Triangle {
+    constructor(a, b, c) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+      if(a + b < c || a + c < b || c + b < a) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+    }
+    get perimeter() {
+      return this.a + this.b + this.c;
+    }
+    
+    get area () {
+        let p = (this.a + this.b + this.c) / 2;
+        return Number(Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c)).toFixed(3));
+    }
+  }
+  
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch  {
+      return {
+        get perimeter() {
+            return "Ошибка! Треугольник не существует";
+        },
+        get area() {
+            return "Ошибка! Треугольник не существует";
+        }
+      }
+    }
+  }
